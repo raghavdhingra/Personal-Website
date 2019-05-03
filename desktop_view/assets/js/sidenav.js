4936051a55077c877f3e3b30108d506aaaa2632f
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-05-03 19:59:09 +0530

    Add files via upload

--- desktop_view/assets/js/sidenav.js
+++ desktop_view/assets/js/sidenav.js
@@ -9,3 +9,22 @@ const open = () => {
     toggle_1.classList.add("close");
     toggle_2.classList.add("close");
 }
+
+const close = () => {
+    sidenav.style.marginLeft="-200px";
+    toggle_1.classList.remove("close");
+    toggle_2.classList.remove("close");
+    toggle_1.classList.add("open");
+    toggle_2.classList.add("open");
+}
+var x = 1;
+document.getElementById("nav-toggle").addEventListener("click",()=>{
+    if (x===1){
+        open();
+        x-=1;
+    }
+    else if (x===0){
+        close();
+        x+=1;
+    }
+});
